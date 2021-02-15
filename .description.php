5930d75f7b6a77c5aad030d480f7760dc0122879
Iv-Alex <web@logycon.ru>
2021-02-15 18:12:56 +0300

    project import

---
+++ .description.php
@@ -0,0 +1,20 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$arComponentDescription = array(
+	// component name (3 level) in components tree
+	"NAME" => GetMessage("SMSN_GOODS_ADDING_CART_LIST"),
+	"DESCRIPTION" => GetMessage("SMSN_GOODS_ADDING_CART_DESC"),
+	"ICON" => "/images/icon.gif",
+	"PATH" => array(
+		"ID" => "samson",
+		// 1 level name in components tree
+		"NAME" => GetMessage("SMSN_GROUP_TITLE"),
+		"CHILD" => array(
+			"ID" => "goods_adding_cart",
+			// 2 level name in components tree
+			"NAME" => GetMessage("SMSN_CART_PARENT_TITLE")
+		)
+	),
+);
+?>
